0adfd8e8c7bf46348bc0c8d5a27341e32a722696
Benjamin Shields <ben.d.shields@gmail.com>
2019-09-03 15:38:44 -0700

    Refactor some design in the Handler type

--- handler.go
+++ handler.go
@@ -1,47 +1,79 @@
 package tftp
 
-import "context"
+import (
+	"context"
+	"time"
+)
+
+type Handler struct {
+	// requestReader listens for new packets from a client.
+	packetReader *Conn
+}
 
 func HandleRequest(ctx context.Context, request Packet, done chan<- error) {
-	clientReader, err := NewConn("127.0.0.1:0") // :0 tells the OS to assign an ephemeral port
-	if err != nil {
+	handler := NewHandler(request)
+	handlerFinished := handler.Start(ctx)
+	select {
+	case err := <-handlerFinished:
 		done <- err
-		return
-	}
-	clientPackets := clientReader.ReadContinuously(ctx)
-	clientDone := make(chan error)
-	clientHandler := NewHandler(ctx, request, clientDone)
-	for {
-		select {
-		case packet := <-clientPackets:
-			clientHandler <- packet
-		case err := <-clientDone:
-			done <- err
-			return
-		case <-ctx.Done():
-			done <- ctx.Err()
-			return
-		}
+	case <-ctx.Done():
+		done <- ctx.Err()
 	}
 }
 
-// TODO: I think this should become its own type and file
-func NewHandler(ctx context.Context, request Packet, out chan<- error) chan<- Packet {
-	in := make(chan Packet)
-	go func() {
+func NewHandler(request Packet) *Handler {
+	handler := &Handler{}
+	return handler
+}
 
+func (handler *Handler) setup() error {
+	err := handler.setupPacketReader()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (handler *Handler) setupPacketReader() error {
+	conn, err := NewConn("127.0.0.1:0") // :0 tells the OS to assign an ephemeral port
+	if err != nil {
+		return err
+	}
+	handler.packetReader = conn
+	return nil
+}
+
+func (handler *Handler) Start(ctx context.Context) <-chan error {
+	out := make(chan error)
+	go func() {
+		err := handler.setup()
+		if err != nil {
+			out <- err
+			// TODO call some handler cleanup func?
+			return
+		}
 		for {
+			ctxTimeout, _ := context.WithTimeout(ctx, 5*time.Second)
+			in := handler.packetReader.Read(ctxTimeout)
 			select {
 			case packet := <-in:
-				go handle(packet)
+				go handler.Handle(packet)
 			case <-ctx.Done():
+				// TODO should I close packetReader here? Send an error packet to client?
+				// TODO call some handler cleanup func?
+				/*// this needs to be deferred
+				err = clientReader.rwc.Close()
+				if err != nil {
+					out <- err
+				}*/
 				return
 			}
 		}
 	}()
-	return in
+	return out
 }
 
-func handle(request Packet) {
+func (handler *Handler) Handle(request Packet) {
 	/* TODO This is a dummy function */
 }
